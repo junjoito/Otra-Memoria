f0f7b405184b6398e3c232574cf361ba159122e3
Juan José Pérez Díaz <75373934+junjoito@users.noreply.github.com>
2021-08-03 18:42:43 -0500

    Nuevo escenario
    Agregamos el segundo mundo con todos los cubos

--- js/main.js
+++ js/main.js
@@ -11,10 +11,12 @@ let control;
 const clock = new THREE.Clock();
 
 const startButton = document.getElementById( 'startButton' );
-startButton.addEventListener( 'click', init );
+startButton.addEventListener( 'click', firstWorld );
 
-function init() {
+const startButton2 = document.getElementById( 'startButton2' );
+startButton2.addEventListener( 'click', secondWorld );
 
+function firstWorld() {
     const overlay = document.getElementById( 'overlay' );
 	overlay.remove();
 
@@ -25,16 +27,13 @@ function init() {
 	camera.add( listener );
 
 	scene = new THREE.Scene();
-	//scene.background = new THREE.Color( 0xcce0ff );
-	scene.background = new THREE.Color(0xc5c0b2);
 
-	/*const loader = new THREE.TextureLoader();
+	const loader = new THREE.TextureLoader();
     loader.load("img/textures/backgraund.jpg",function(texture){
       scene.background = texture;
-    });*/
+    });
 
-	const sphere = new THREE.BoxGeometry( 80, 80, 80 );
-	const orbe = new THREE.SphereGeometry(2, 16, 16);
+	const sphere = new THREE.BoxGeometry( 60, 60, 60 );
 	//Texturas de cada geometria
 
 	const imagePrefix = "img/scenario_01/iglesia_";
@@ -109,12 +108,9 @@ function init() {
 	const audioLoader = new THREE.AudioLoader();
 
 	cube1 = new THREE.Mesh( sphere, skyMaterial );
-	cube1.position.set( - 250, 40, 0 );
+	cube1.position.set( - 450, 35, 0 );
 	scene.add( cube1 );
-
-	/*const moon= new THREE.Mesh( orbe, moonMaterial);
-	moon.position.set( - 350, 40, 0 );
-	scene.add( moon);*/
+	
 	const sound1 = new THREE.PositionalAudio( listener );
 	audioLoader.load( 'sounds/Misa.mp3', function ( buffer ) {
 		sound1.setBuffer( buffer );
@@ -126,7 +122,7 @@ function init() {
 	cube1.add( sound1 );
 			
 	cube2 = new THREE.Mesh( sphere, skyMaterial2 );
-	cube2.position.set( 0, 40, - 350 );
+	cube2.position.set( 0, 35, - 450 );
 	scene.add( cube2 );
 
 	const sound2 = new THREE.PositionalAudio( listener );
@@ -142,7 +138,7 @@ function init() {
 	cube2.add( sound2 );
 
 	cube3 = new THREE.Mesh( sphere, skyMaterial3 );
-	cube3.position.set( 350, 40, 0 );
+	cube3.position.set( 450, 35, 0 );
 	scene.add( cube3 );
 
 	const sound3 = new THREE.PositionalAudio( listener );
@@ -158,11 +154,11 @@ function init() {
 	cube3.add( sound3 );
 
 	cube4 = new THREE.Mesh( sphere, skyMaterial4 );
-	cube4.position.set( 0, 40, 350 );
+	cube4.position.set( 450, 35, - 450 );
 	scene.add( cube4 );
 
 	cube5 = new THREE.Mesh( sphere, skyMaterial5 );
-	cube5.position.set( - 350, 40, 350 );
+	cube5.position.set( 0, 35, 450 );
 	scene.add( cube5 );
 	/*const sound3 = new THREE.PositionalAudio( listener );
 	const oscillator = listener.context.createOscillator();
@@ -186,7 +182,7 @@ function init() {
 
 	} );
 
-	const floorTexture = new THREE.ImageUtils.loadTexture( 'img/textures/floor.jpg' );
+	/*const floorTexture = new THREE.ImageUtils.loadTexture( 'img/textures/floor.jpg' );
 	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
 	floorTexture.repeat.set( 10, 10 );
 	// DoubleSide: render texture on both sides of mesh
@@ -195,7 +191,11 @@ function init() {
 	const floor = new THREE.Mesh(floorGeometry, floorMaterial);
 	floor.position.y = -0.5;
 	floor.rotation.x = Math.PI / 2;
-	scene.add(floor);
+	scene.add(floor);*/
+
+	const helper = new THREE.GridHelper( 1000, 40, 0xc5c0b2, 0xc5c0b2 );
+	helper.position.y = 0.1;
+	scene.add( helper );
 
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setPixelRatio( window.devicePixelRatio );
@@ -205,14 +205,187 @@ function init() {
 	//Controles
 
 	controls = new FirstPersonControls( camera, renderer.domElement );
+	controls.movementSpeed = 150;
+	controls.lookSpeed = 0.1;
+	controls.lookVertical = false;
 
-	controls.movementSpeed = 70;
-	controls.lookSpeed = 0.05;
-	controls.noFly = true;
-	//controls.lookVertical = false;
-	controls.lookVertical = true;
+	window.addEventListener( 'resize', onWindowResize );
+	animate();
 
-	//
+}
+
+function secondWorld() {
+    const overlay = document.getElementById( 'overlay' );
+	overlay.remove();
+
+	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.set( 0, 35, 0 );
+
+	const listener = new THREE.AudioListener();
+	camera.add( listener );
+
+	scene = new THREE.Scene();
+
+	const loader = new THREE.TextureLoader();
+    loader.load("img/textures/backgraund_2.jpg",function(texture){
+      scene.background = texture;
+    });
+
+	const sphere = new THREE.BoxGeometry( 60, 60, 60 );
+	//Texturas de cada geometria
+
+	const imagePrefix = "img/scenario_06/in-iglesia_";
+	const directions  = ["ft", "bk", "up", "dn", "rt", "lf"];
+	const imageSuffix = ".jpg";
+
+	const materialArray = [];
+	for (var i = 0; i < 6; i++)
+		materialArray.push( new THREE.MeshBasicMaterial({
+		map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+		side: THREE.BackSide
+	}));
+	const skyMaterial = new THREE.MeshFaceMaterial( materialArray );
+
+	const imagePrefix2 = "img/scenario_07/habitacion_";
+	const directions2  = ["ft", "bk", "up", "dn", "rt", "lf"];
+	const imageSuffix2 = ".jpg";
+
+	const materialArray2 = [];
+	for (var i = 0; i < 6; i++)
+		materialArray2.push( new THREE.MeshBasicMaterial({
+		map: THREE.ImageUtils.loadTexture( imagePrefix2 + directions2[i] + imageSuffix2 ),
+		side: THREE.BackSide
+	}));
+	const skyMaterial2 = new THREE.MeshFaceMaterial( materialArray2 );
+
+	const imagePrefix3 = "img/scenario_08/exterior_";
+	const directions3  = ["ft", "bk", "up", "dn", "rt", "lf"];
+	const imageSuffix3 = ".jpg";
+
+	const materialArray3 = [];
+	for (var i = 0; i < 6; i++)
+		materialArray3.push( new THREE.MeshBasicMaterial({
+		map: THREE.ImageUtils.loadTexture( imagePrefix3 + directions3[i] + imageSuffix3 ),
+		side: THREE.BackSide
+	}));
+	const skyMaterial3 = new THREE.MeshFaceMaterial( materialArray3 );
+
+	const imagePrefix4 = "img/scenario_09/baño_";
+	const directions4  = ["ft", "bk", "up", "dn", "rt", "lf"];
+	const imageSuffix4 = ".jpg";
+
+	const materialArray4 = [];
+	for (var i = 0; i < 6; i++)
+		materialArray4.push( new THREE.MeshBasicMaterial({
+		map: THREE.ImageUtils.loadTexture( imagePrefix4 + directions4[i] + imageSuffix4 ),
+		side: THREE.BackSide
+	}));
+	const skyMaterial4 = new THREE.MeshFaceMaterial( materialArray4 );
+
+	const imagePrefix5 = "img/scenario_10/patio_";
+	const directions5  = ["ft", "bk", "up", "dn", "rt", "lf"];
+	const imageSuffix5 = ".jpg";
+
+	const materialArray5 = [];
+	for (var i = 0; i < 6; i++)
+		materialArray5.push( new THREE.MeshBasicMaterial({
+		map: THREE.ImageUtils.loadTexture( imagePrefix5 + directions5[i] + imageSuffix5 ),
+		side: THREE.BackSide
+	}));
+	const skyMaterial5 = new THREE.MeshFaceMaterial( materialArray5 );
+
+	const logoTexture = new THREE.TextureLoader().load('img/logo/La Otra Memoria.png');
+		
+	const moonTexture = new THREE.TextureLoader().load('img/textures/moon.jpg');
+	const moonMaterial = new THREE.MeshBasicMaterial({ map: moonTexture });
+	// Efectos de Sonido para cada geometria
+	control = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5), new THREE.MeshBasicMaterial({ map: logoTexture, side: THREE.DoubleSide }));
+	control.position.set( 0, 40, - 50 );
+	scene.add( control );
+
+	const audioLoader = new THREE.AudioLoader();
+
+	cube1 = new THREE.Mesh( sphere, skyMaterial );
+	cube1.position.set( - 450, 35, 0 );
+	scene.add( cube1 );
+	
+	const sound1 = new THREE.PositionalAudio( listener );
+	audioLoader.load( '', function ( buffer ) {
+		sound1.setBuffer( buffer );
+		sound1.setLoop( true );
+		sound1.setVolume( 1.5 );
+		sound1.setRefDistance( 5 )
+		sound1.play();	
+	} );
+	cube1.add( sound1 );
+			
+	cube2 = new THREE.Mesh( sphere, skyMaterial2 );
+	cube2.position.set( 0, 35, - 450 );
+	scene.add( cube2 );
+
+	const sound2 = new THREE.PositionalAudio( listener );
+	audioLoader.load( '', function ( buffer ) {
+
+		sound2.setBuffer( buffer );
+		sound2.setLoop( true );
+		sound2.setRefDistance( 5 );
+		sound2.setVolume(1.5);
+		sound2.play();
+
+	} );
+	cube2.add( sound2 );
+
+	cube3 = new THREE.Mesh( sphere, skyMaterial3 );
+	cube3.position.set( 450, 35, 0 );
+	scene.add( cube3 );
+
+	const sound3 = new THREE.PositionalAudio( listener );
+	audioLoader.load( '', function ( buffer ) {
+
+		sound3.setBuffer( buffer );
+		sound3.setLoop( true );
+		sound3.setRefDistance( 5 );
+		sound3.setVolume(1.5);
+		sound3.play();
+
+	} );
+	cube3.add( sound3 );
+
+	cube4 = new THREE.Mesh( sphere, skyMaterial4 );
+	cube4.position.set( 450, 35, - 450 );
+	scene.add( cube4 );
+
+	cube5 = new THREE.Mesh( sphere, skyMaterial5 );
+	cube5.position.set( 0, 35, 450 );
+	scene.add( cube5 );
+
+	// Sonido de Ambientacion
+
+	const sound4 = new THREE.Audio( listener );
+	audioLoader.load( 'sounds/Project_Utopia.ogg', function ( buffer ) {
+
+		sound4.setBuffer( buffer );
+		sound4.setLoop( true );
+		sound4.setVolume( 1 );
+		sound4.play();
+
+	} );
+
+	const helper = new THREE.GridHelper( 1000, 20, 0xbe4554, 0xbe4554 );
+	helper.position.y = 0.1;
+	scene.add( helper );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	//Controles
+
+	controls = new FirstPersonControls( camera, renderer.domElement );
+	controls.movementSpeed = 150;
+	controls.lookSpeed = 0.1;
+	controls.lookVertical = false;
 
 	window.addEventListener( 'resize', onWindowResize );
 	animate();
@@ -234,16 +407,9 @@ function onWindowResize() {
 function animate() {
 
 	requestAnimationFrame( animate );
-	cube1.rotation.y += 0.001;
-	cube2.rotation.y += 0.001;
-				
 	control.rotation.x += 0.01;
 	control.rotation.y += 0.01;
 	control.rotation.z += 0.01;
-
-	cube3.rotation.y += 0.001;
-	cube4.rotation.y += 0.001;
-	cube5.rotation.y += 0.001;
 	render();
 
 }
